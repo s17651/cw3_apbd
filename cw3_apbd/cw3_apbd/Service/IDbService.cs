896cd5dadb1b867a59bb9df17f64773b0facad52
nanaki <grzegorz.pawlaszek@gmail.com>
2020-04-04 23:22:26 +0200

    cw3

---
+++ cw3_apbd/cw3_apbd/Service/IDbService.cs
@@ -0,0 +1,13 @@
+﻿using cw3_apbd.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw3_apbd.Service
+{
+    public interface IDbService
+    {
+        public IEnumerable<Student> GetStudents();
+    }
+}
---
+++ cw3_apbd/cw3_apbd/Service/MockDbService.cs
@@ -0,0 +1,27 @@
+﻿using cw3_apbd.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw3_apbd.Service
+{
+    public class MockDbService : IDbService
+    {
+        private static IEnumerable<Student> _students;
+
+        static MockDbService()
+        {
+            _students = new List<Student>
+            {
+                new Student{IdStudent=1, FirstName="Jan", LastName="Kowalski"},
+                new Student{IdStudent=2, FirstName="Anna", LastName="Majewski"},
+                new Student{IdStudent=3, FirstName="Andrzej", LastName="Andrzejewicz"}
+            };
+        }
+        public IEnumerable<Student> GetStudents()
+        {
+            return _students;
+        }
+    }
+}
